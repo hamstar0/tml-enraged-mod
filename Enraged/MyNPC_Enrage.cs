32aeeda09a36123d90d1bc5e05078f70d1c8169a
hamstar0 <webutek@gmail.com>
2020-05-10 13:09:45 -0700

    v0.3.0 - Added screen shake effect from nearby enraged boss

--- Enraged/MyNPC_Enrage.cs
+++ Enraged/MyNPC_Enrage.cs
@@ -5,6 +5,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using HamstarHelpers.Tiles;
 using HamstarHelpers.Helpers.XNA;
+using HamstarHelpers.Services.Camera;
 using Enraged.Buffs;
 
 
@@ -59,15 +60,32 @@ namespace Enraged {
 		public override void DrawEffects( NPC npc, ref Color drawColor ) {
 			if( npc.boss ) {
 				if( npc.HasBuff(ModContent.BuffType<EnragedBuff>()) ) {
-					// Add red tint
-					var newColor = new Color( 255, 128, 128 );
-					drawColor = XNAColorHelpers.Mul( drawColor, newColor );
-
-					// Add shakes
-					npc.scale = ((Main.rand.NextFloat() * 0.2f) - 0.1f) + this.BaseScale;
+					this.ApplyVisualFx( npc, ref drawColor );
 				} else {
 					this.BaseScale = npc.scale;
 				}
+			}
+		}
+
+		////
+
+		private void ApplyVisualFx( NPC npc, ref Color drawColor ) {
+			// Add red tint
+			var newColor = new Color( 255, 128, 128 );
+			drawColor = XNAColorHelpers.Mul( drawColor, newColor );
+
+			// Add NPC vibration
+			npc.scale = ( ( Main.rand.NextFloat() * 0.2f ) - 0.1f ) + this.BaseScale;
+
+			// Add screen shake
+			float dist = (npc.Center - Main.LocalPlayer.Center).Length();
+			float magnitudePercent = 1f - (dist / 768f);
+			float magnitude = 8f * magnitudePercent;
+			//magnitude *= Main.LocalPlayer.Center.Y == 0f
+			//	? magnitude
+			//	: magnitude * 4f;
+			if( magnitude > 0f ) {
+				Camera.ApplyShake( magnitude, 30, 15 );
 			}
 		}
 	}
